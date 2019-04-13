54d4b17cc45fba4ed1406d25a02a96fecb8cfc8d
Addarsh Chandrasekar <addarsh@Addarshs-MacBook-Pro.local>
2019-04-13 07:47:59 -0700

    Updated training script, removed dataset and checkpoint dirs
    1. Script can download/load dataset/checkpoint from given location
    2. Added ipython notebook to visualize data

--- mrnn.py
+++ mrnn.py
@@ -15,27 +15,34 @@ import datetime
 import numpy as np
 import argparse
 import skimage.draw
+import boto3
+import socket
+from PIL import Image
+
+if socket.gethostname() == "Addarshs-MacBook-Pro.local":
+  volume_mount_dir = "/Users/addarsh/virtualenvs/aws_train/dltraining"
+else:
+  volume_mount_dir = '/dltraining/'
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("Mask_RCNN")
-DATASET_DIR = os.path.abspath("resized")
+ROOT_DIR = "Mask_RCNN"
+DATASET_DIR = os.path.join(volume_mount_dir, "dataset")
+CHECKPOINT_DIR = os.path.join(volume_mount_dir, "checkpoints")
+SPLIT_FILE = os.path.join(DATASET_DIR, "data_split.json")
+# Path to trained weights file
+COCO_WEIGHTS_PATH = os.path.abspath(os.path.join(volume_mount_dir, "mask_rcnn_coco.h5"))
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
 from mrcnn.config import Config
 from mrcnn import model as modellib, utils
 
-# Path to trained weights file
-COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
-
-# Directory to save logs and model checkpoints, if not provided
-# through the command line argument --logs
-DEFAULT_LOGS_DIR = os.path.join("checkpoints")
-
-class_dict = {'eye': 1, 'eyebrow': 2, 'nose': 3, 'upper lip': 4, 'lower lip': 5,
-'face': 6, 'neck': 7, 'hair': 8, 'glasses': 9, 'ear': 10, 'teeth': 11,
-'beard': 12, 'hat': 13, 'french beard': 14, 'sunglasses': 15, 'tongue': 16,
-'goatee': 17, 'hoodie': 18, 'moustache': 19, 'stubble': 20, 'closed eye': 21}
+# Dictionary of labels for training.
+class_dict = {
+  "Eye (Open)": 1, "Eyeball": 2, "Eyebrow": 3, "Reading glasses": 4, "Sunglasses": 5, "Eye (Closed)": 6,
+  "Nose": 7, "Nostril": 8, "Upper Lip":9, "Lower Lip":10, "Teeth":11, "Tongue": 12, "Facial Hair":13,
+  "Face": 14, "Hair (on head)": 15, "Head Gear": 16, "Bald Head": 17, "Ear": 18
+}
 
 ############################################################
 #  Configurations
@@ -54,13 +61,16 @@ class FaceConfig(Config):
     IMAGES_PER_GPU = 2
 
     # Number of classes (including background)
-    NUM_CLASSES = 21 + 1  # Background + class_dict
+    NUM_CLASSES = 18 + 1  # Background + class_dict
 
     # Number of training steps per epoch
     STEPS_PER_EPOCH = 100
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
+
+    # Remove mini mask to improve accuracy.
+    USE_MINI_MASK = False
 
 
 ############################################################
@@ -69,9 +79,8 @@ class FaceConfig(Config):
 
 class FaceDataset(utils.Dataset):
 
-    def load_faces(self, dataset_dir, subset):
+    def load_faces(self, subset):
         """Load a subset of the Balloon dataset.
-        dataset_dir: Root directory of the dataset.
         subset: Subset to load: train or val
         """
         source = "face"
@@ -81,28 +90,47 @@ class FaceDataset(utils.Dataset):
 
         # Train or validation dataset?
         assert subset in ["train", "val"]
-        img_dir = os.path.join(dataset_dir, subset)
 
-        for f in os.listdir(img_dir):
-          if not f.endswith(".jpg") and not f.endswith(".png"):
-            continue
-          label_dir = os.path.join(dataset_dir, "labels")
-          an = json.load(open(os.path.join(label_dir, os.path.splitext(f)[0] + ".json")))
-          polygons = []
-          for v in an["shapes"]:
-            all_x = [p[0] for p in v["points"]]
-            all_y = [p[1] for p in v["points"]]
-            polygons.append({"all_points_x":all_x, "all_points_y":all_y, "label": v["label"]})
+        # Load split annotations.
+        split_ann = []
+        with open(SPLIT_FILE, "r") as g:
+          split_ann = json.load(g)
+        want_set = set(split_ann[subset])
 
-          image_path = os.path.join(img_dir, f)
-          self.add_image(
-            source=source,
-            image_id=f, # filename used for annotation.
-            path=image_path,
-            width=an["imageWidth"],
-            height=an["imageHeight"],
-            polygons=polygons,
-          )
+        for i in range(1,2):
+          for j in range(1,2):
+            base_dir = os.path.join("helen_r"+str(i),"50_"+str(j))
+            for f in os.listdir(os.path.join(DATASET_DIR, base_dir)):
+              if not f.endswith(".jpg"):
+                continue
+              if os.path.join(base_dir, f) not in want_set:
+                continue
+              img_path = os.path.join(DATASET_DIR, os.path.join(base_dir, f))
+
+              # Get image annotations.
+              polygons = []
+              merged_dir = os.path.join(DATASET_DIR, os.path.join("merged", base_dir))
+              for c in ["eye", "nose", "face"]:
+                ann_path = os.path.join(os.path.join(merged_dir, c), os.path.splitext(f)[0]+".json" )
+                ann = json.load(open(ann_path))
+                for d in ann:
+                  if d["class"] not in class_dict:
+                    print ("Skipping polygon with class: ", d["class"], " in file: ", img_path)
+                    continue
+                  polygons.append(d)
+
+              # Get image width and height.
+              im = Image.open(img_path)
+              width, height = im.size
+
+              self.add_image(
+                source=source,
+                image_id=f, # filename used for annotation.
+                path=img_path,
+                width=width,
+                height=height,
+                polygons=polygons,
+              )
 
     def load_mask(self, image_id):
         """Generate instance masks for an image.
@@ -111,7 +139,7 @@ class FaceDataset(utils.Dataset):
             one mask per instance.
         class_ids: a 1D array of class IDs of the instance masks.
         """
-        # If not a balloon dataset image, delegate to parent class.
+        # If not a face dataset image, delegate to parent class.
         image_info = self.image_info[image_id]
         if image_info["source"] != "face":
           raise Exception("source is not face in load_mask")
@@ -122,19 +150,19 @@ class FaceDataset(utils.Dataset):
         mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
                         dtype=np.uint8)
         class_ids = np.ones(len(info["polygons"]), dtype=np.int32)
-        for i, p in enumerate(info["polygons"]):
-            # Get indexes of pixels inside the polygon and set them to 1
-            rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
-            rr = self.bounds(rr, info["height"])
-            cc = self.bounds(cc, info["width"])
+        for i, c in enumerate(info["polygons"]):
+          # Get indices of pixels inside the polygon and set them to 1
+          for points in c["data"]:
+            x = [p[0] for p in points]
+            y = [p[1] for p in points]
+
+            rr, cc = skimage.draw.polygon(y, x, shape=(info["height"] , info["width"]))
             mask[rr, cc, i] = 1
-            class_ids[i] = class_dict[p["label"]]
+
+          class_ids[i] = class_dict[c["class"]]
 
         # Return mask, and array of class IDs of each instance.
         return mask.astype(np.bool), class_ids
-
-    def bounds(self, indices, ub):
-      return [min(max(0, idx), ub-1) for idx in indices]
 
     def image_reference(self, image_id):
         """Return the path of the image."""
@@ -143,19 +171,18 @@ class FaceDataset(utils.Dataset):
           raise Exception("source is not face in image_reference")
         return info["path"]
 
-
 def train(model):
     """Train the model."""
     print ("Training starts")
 
     # Training dataset.
     dataset_train = FaceDataset()
-    dataset_train.load_faces(args.dataset, "train")
+    dataset_train.load_faces("train")
     dataset_train.prepare()
 
     # Validation dataset
     dataset_val = FaceDataset()
-    dataset_val.load_faces(args.dataset, "val")
+    dataset_val.load_faces("val")
     dataset_val.prepare()
 
     # *** This training schedule is an example. Update to your needs ***
@@ -194,6 +221,63 @@ def color_splash(image, mask, class_ids):
         splash = gray.astype(np.uint8)
     return splash
 
+"""
+download_dataset will fetch the face annotation dataset from Amazon S3 bucket.
+This is in the event that the new instance is assigned a different zone
+than the elastic block.
+"""
+def download_dataset():
+  s3 = boto3.resource('s3')
+  bucket_name = "addarsh-face-segment"
+
+  bucket = s3.Bucket(bucket_name)
+
+  remote_merged_dir = "merged"
+  if not os.path.exists(DATASET_DIR):
+    os.makedirs(DATASET_DIR)
+
+  # Download the split file.
+  split_file = "data_split.json"
+  for object_summary in bucket.objects.filter(Prefix=split_file):
+    bucket.download_file(object_summary.key, os.path.join(DATASET_DIR, object_summary.key))
+
+  for i in range(1,2):
+    for j in range(1,2):
+      download_img_and_annotation(os.path.join("helen_r"+str(i),"50_"+str(j)), remote_merged_dir, bucket)
+      break
+    break
+
+"""
+download_img_and_annotation downloads images in given directory and corresponding merged annotations.
+"""
+def download_img_and_annotation(remote_dir, remote_merged_dir, bucket):
+  local_dir = os.path.join(DATASET_DIR, remote_dir)
+  if not os.path.exists(local_dir):
+    os.makedirs(local_dir)
+
+  local_merged_dir = os.path.join(os.path.join(DATASET_DIR, remote_merged_dir), remote_dir)
+  for c in ["eye", "nose", "face"]:
+    if not os.path.exists(os.path.join(local_merged_dir,c)):
+      os.makedirs(os.path.join(local_merged_dir,c))
+
+  # Download image file.
+  for object_summary in bucket.objects.filter(Prefix=remote_dir+"/"):
+    if not os.path.split(object_summary.key)[1].endswith(".jpg"):
+      continue
+    if os.path.exists(os.path.join(DATASET_DIR,object_summary.key)):
+      continue
+    bucket.download_file(object_summary.key, os.path.join(DATASET_DIR,object_summary.key))
+
+  # Download associated merged annotation files.
+  for c in ["eye", "nose", "face"]:
+    for object_summary in bucket.objects.filter(Prefix=os.path.join(os.path.join(remote_merged_dir, remote_dir), c)):
+      if not os.path.split(object_summary.key)[1].endswith(".json"):
+        continue
+      if os.path.exists(os.path.join(DATASET_DIR,object_summary.key)):
+        continue
+      bucket.download_file(object_summary.key, os.path.join(DATASET_DIR,object_summary.key))
+
+
 def detect(model, image_path):
   print("Running on {}".format(args.image))
   # Read image
@@ -208,23 +292,31 @@ def detect(model, image_path):
   file_name = "splash_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
   skimage.io.imsave(file_name, splash)
 
+"""
+epoch_path returns the latest epoch file.
+"""
+def epoch_path(list_of_checkpoint_files):
+  max_epoch_number = 1
+  max_epoch_file = ""
+  for f in list_of_checkpoint_files:
+    if not f.startswith("mask_rcnn_face"):
+      continue
+    a = f.split(".")[0]
+    num = int(a.split("_")[-1])
+    if num > max_epoch_number:
+      max_epoch_number = num
+      max_epoch_file = f
+  if max_epoch_file == "":
+    raise Exception("Max epoch file cannot be empty")
+  return max_epoch_file
+
 if __name__ == '__main__':
   # Parse command line arguments
   parser = argparse.ArgumentParser(
       description='Train Mask R-CNN to detect balloons.')
-  parser.add_argument("command",
+  parser.add_argument("--command",
                       metavar="<command>",
                       help="'train' or 'detect'")
-  parser.add_argument('--dataset', required=False,
-                      metavar="/path/to/balloon/dataset/",
-                      help='Directory of the Balloon dataset')
-  parser.add_argument('--weights', required=True,
-                      metavar="/path/to/weights.h5",
-                      help="Path to weights .h5 file or 'coco'")
-  parser.add_argument('--logs', required=False,
-                      default=DEFAULT_LOGS_DIR,
-                      metavar="/path/to/logs/",
-                      help='Logs and checkpoints directory (default=logs/)')
   parser.add_argument('--image', required=False,
                       metavar="path or URL to image",
                       help='Image to apply the color splash effect on')
@@ -232,14 +324,16 @@ if __name__ == '__main__':
 
   # Validate arguments
   if args.command == "train":
-      assert args.dataset, "Argument --dataset is required for training"
+    # Download dataset if it does not exist.
+    if not os.path.exists(DATASET_DIR):
+      print ("Dataset does not exist, downloading....")
+      download_dataset()
   elif args.command == "detect":
       assert args.image,\
              "Provide --image or --video to apply color splash"
 
-  print("Weights: ", args.weights)
-  print("Dataset: ", args.dataset)
-  print("Logs: ", args.logs)
+  print("Dataset: ", DATASET_DIR)
+  print("Logs: ", CHECKPOINT_DIR)
 
   # Configurations
   if args.command == "train":
@@ -256,29 +350,28 @@ if __name__ == '__main__':
   # Create model
   if args.command == "train":
       model = modellib.MaskRCNN(mode="training", config=config,
-                                model_dir=args.logs)
+                                model_dir=CHECKPOINT_DIR)
   else:
       model = modellib.MaskRCNN(mode="inference", config=config,
-                                model_dir=args.logs)
+                                model_dir=CHECKPOINT_DIR)
 
   # Select weights file to load
-  if args.weights.lower() == "coco":
-      weights_path = COCO_WEIGHTS_PATH
-      # Download weights file
-      if not os.path.exists(weights_path):
-          utils.download_trained_weights(weights_path)
-  elif args.weights.lower() == "last":
-      # Find last trained weights
-      weights_path = model.find_last()
-  elif args.weights.lower() == "imagenet":
-      # Start from ImageNet trained weights
-      weights_path = model.get_imagenet_weights()
-  else:
-      weights_path = args.weights
+  weights_path = ""
+  try:
+    weights_path = model.find_last()
+    print ("Using last iteration weights")
+  except Exception as e:
+    # Load Coco weights.
+    weights_path = COCO_WEIGHTS_PATH
+    # Download weights file
+    if not os.path.exists(weights_path):
+      print ("Downloading Coco weights...")
+      utils.download_trained_weights(weights_path)
+    print ("Using Coco weights")
 
   # Load weights
   print("Loading weights ", weights_path)
-  if args.weights.lower() == "coco":
+  if weights_path == COCO_WEIGHTS_PATH:
       # Exclude the last layers because they require a matching
       # number of classes
       model.load_weights(weights_path, by_name=True, exclude=[
