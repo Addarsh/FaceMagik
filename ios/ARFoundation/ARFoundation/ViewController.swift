22ca5ec3a92abe7b73cc496d535c771f85d1618a
Addarsh Chandrasekar <addarsh@Addarshs-MacBook-Pro.local>
2020-01-28 18:15:55 -0800

    Implemented uploading face geometry projection to server
    Additional changes:
    1. Verfied that the camera projection is correct for the 3D mesh points
    2. Implemented spherical harmonics coefficient and color temperature upload to server.
    3. Implemented normal calculation and upload to server.

--- ios/ARFoundation/ARFoundation/ViewController.swift
+++ ios/ARFoundation/ARFoundation/ViewController.swift
@@ -23,8 +23,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     private var pngData: Data?
     
-    private let uploadURL = "http://[2600:1700:f1b0:6400:4483:9bfe:6730:9534]:8000/skin/"
+    private let uploadURL = "http://[2601:647:4200:af70:a94d:5325:6b1c:b020]:8000/skin/"
     
+    private var lastFaceAnchor: ARFaceAnchor?
+    
+    private var lastCamera: ARCamera?
+    
+    private var lastLightEstimate: ARDirectionalLightEstimate?
+    
+    private var lightDict: [String: [Float]]?
+    
+    private var vertices: [[Int]]?
+    
+    private var normals: [[Float]]?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -106,25 +118,55 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             guard let self = self else {
                 return
             }
+            
+            self.lastFaceAnchor = faceAnchor
+            self.lastCamera = self.sceneView.session.currentFrame?.camera
             self.lastCapturedImage = self.sceneView.session.currentFrame?.capturedImage
+            self.lastLightEstimate = self.sceneView.session.currentFrame?.lightEstimate as? ARDirectionalLightEstimate
         }
     }
     
     @IBAction func onClick(_ sender: Any) {
-        //performSegue(withIdentifier: "ShowImage", sender: self)
         concurrentPhotoQueue.sync {
             guard let lastImage = self.lastCapturedImage else {
                 return
             }
             self.pngData =  UIImage(pixelBuffer: lastImage)?.pngData()
+            self.vertices = projectedVertices()
+            self.normals = calcNormals()
+            self.lightDict = lightEstimate()
         }
         
         guard let pngData = self.pngData else {
             return
         }
+        guard let vertices = self.vertices else {
+            return
+        }
+        guard let lightDict = self.lightDict else {
+            return
+        }
+        guard let normals = self.normals else {
+            return
+        }
+        
+        guard let jsonVertices = try? JSONSerialization.data(withJSONObject: vertices) else {
+            return
+        }
+        
+        guard let jsonLight = try? JSONSerialization.data(withJSONObject: lightDict) else {
+            return
+        }
+        
+        guard let jsonNormals =  try? JSONSerialization.data(withJSONObject: normals) else {
+            return
+        }
         
         Alamofire.upload(multipartFormData: { multipartFormData in
             multipartFormData.append(pngData.base64EncodedData(), withName: "fileset", mimeType: "image/png")
+            multipartFormData.append(jsonVertices, withName: "vertices")
+            multipartFormData.append(jsonLight, withName: "lighting")
+            multipartFormData.append(jsonNormals, withName: "normals")
         }, to: uploadURL) { result in
             switch result {
             case .success(let upload, _, _):
@@ -139,6 +181,123 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 print (encodingError)
             }
         }
+    }
+    
+    func lightEstimate() -> [String:[Float]]? {
+        guard let lightEstimate = self.lastLightEstimate else {
+            return nil
+        }
+        let spHarmonics = lightEstimate.sphericalHarmonicsCoefficients
+        var lightDict: [String:[Float]] = [:]
+        lightDict["colorTemperature"] = [Float(lightEstimate.ambientColorTemperature)]
+        
+        let floatSz = 4 // 4 bytes.
+        let blockSz = 36 // 36 bytes for each channel.
+        
+        var redArr: [Float] = []
+        for offset in stride(from: 0, to: blockSz, by: floatSz) {
+            redArr.append(spHarmonics.withUnsafeBytes{ $0.load(fromByteOffset: offset, as: Float.self)})
+        }
+        lightDict["red"] = redArr
+        
+        var greenArr: [Float] = []
+        for offset in stride(from: blockSz, to: 2*blockSz, by: floatSz) {
+            greenArr.append(spHarmonics.withUnsafeBytes{ $0.load(fromByteOffset: offset, as: Float.self)})
+        }
+        lightDict["green"] = greenArr
+        
+        var blueArr: [Float] = []
+        for offset in stride(from: 2*blockSz, to: 3*blockSz, by: floatSz) {
+            blueArr.append(spHarmonics.withUnsafeBytes{ $0.load(fromByteOffset: offset, as: Float.self)})
+        }
+        lightDict["blue"] = blueArr
+        return lightDict
+    }
+    
+    func projectedVertices() -> [[Int]]? {
+        guard let image = self.lastCapturedImage else {
+            return nil
+        }
+        guard let faceAnchor = self.lastFaceAnchor else {
+            return nil
+        }
+        guard let camera = self.lastCamera else {
+            return nil
+        }
+        let geometry = faceAnchor.geometry
+        let vertices = geometry.vertices
+        
+        let width = CVPixelBufferGetWidth(image)
+        let height = CVPixelBufferGetHeight(image)
+        
+        let textureCoordinates = vertices.map { vertex -> [Int] in
+            let world_vector3 = toWorldCords(vertex: vertex, faceAnchor: faceAnchor)
+            let pt = camera.projectPoint(world_vector3,
+                orientation: .portrait,
+                viewportSize: CGSize(
+                    width: CGFloat(height),
+                    height: CGFloat(width)))
+            return [Int(pt.y), Int(pt.x)]
+        }
+        return textureCoordinates
+    }
+    
+    // toWorldCords converts given face vertex from face
+    // coordinate system to world coordinates.
+    func toWorldCords(vertex: vector_float3, faceAnchor: ARFaceAnchor) -> simd_float3 {
+        let vertex4 = vector_float4(vertex.x, vertex.y, vertex.z, 1)
+        let world_vertex4 = simd_mul(faceAnchor.transform, vertex4)
+        return simd_float3(x: world_vertex4.x, y: world_vertex4.y, z: world_vertex4.z)
+    }
+    
+    func calcNormals() -> [[Float]]? {
+        guard let faceAnchor = self.lastFaceAnchor else {
+            return nil
+        }
+        
+        //let vertices = faceAnchor.geometry.vertices
+        let triangleIndices = faceAnchor.geometry.triangleIndices
+        let triangleCount = faceAnchor.geometry.triangleCount
+        let vertices = faceAnchor.geometry.vertices
+        
+        var count = 0
+        var neighborMap = [Int:[Set<Int>]]()
+        
+        // Map vertex to neighgboring faces.
+        while count != triangleCount {
+            let face: Set<Int> = [Int(triangleIndices[count*3]), Int(triangleIndices[count*3 + 1]), Int(triangleIndices[count*3 + 2])]
+            face.forEach { index  in
+                if neighborMap[index] != nil {
+                    neighborMap[index]!.append(face)
+                } else {
+                    neighborMap[index] = [face]
+                }
+            }
+            count += 1
+        }
+        
+        // Calculate normal for each vertex.
+        let normals = vertices.enumerated().map { (index, _) -> [Float] in
+            let faces = neighborMap[index]!
+            var nList: [simd_float3] = []
+            for face in faces {
+                var simdArr: [simd_float3] = []
+                for vIdx in face {
+                    let v = toWorldCords(vertex: vertices[vIdx], faceAnchor: faceAnchor)
+                    simdArr.append(simd_float3(v.x, v.y, v.z))
+                }
+                let vector1 = simdArr[1] - simdArr[2]
+                let vector2 = simdArr[1] - simdArr[0]
+                nList.append(simd_normalize(simd_cross(vector1, vector2)))
+            }
+            var avgNormal: simd_float3 = simd_float3(0, 0, 0)
+            for n in nList {
+                avgNormal += n
+            }
+            avgNormal = simd_normalize(avgNormal/Float(nList.count))
+            return [Float(avgNormal[0]), Float(avgNormal[1]), Float(avgNormal[2])]
+        }
+        return normals
     }
     
     // prepare is run just before segue. It captures last stored image
--- server/skin/views.py
+++ server/skin/views.py
@@ -3,18 +3,28 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http.multipartparser import MultiPartParser
 from PIL import Image
 import io
+import numpy as np
 import base64
+import json
+import cv2
 
 @csrf_exempt
 def index(request):
   if request.method == "POST":
     data, files = MultiPartParser(request.META, io.BytesIO(request.body),
                         request.upload_handlers, "utf-8").parse()
-    with open("Output.txt", "w") as text_file:
-      print(data["fileset"], file=text_file)
+
+    faceVertices = json.loads(data["vertices"])
+    lighting = json.loads(data["lighting"])
+    normals = json.loads(data["normals"])
 
     image = Image.open(io.BytesIO(base64.b64decode(data["fileset"])))
     image = image.transpose(Image.ROTATE_270)
+    image = np.asarray(image, dtype="uint8")
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    image.save("test.png", "PNG")
+    for v in faceVertices:
+      image[v[0], v[1]] = [0, 255, 0]
+
+    cv2.imwrite("test.png", image)
     return JsonResponse({"message": "Upload complete."}, status=200)
